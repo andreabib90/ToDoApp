d729255da16e1353c5a2d59ae1162ea1dcf055bd
dkabib <dk_abib@hotmail.com>
2024-04-02 21:32:55 -0600

    Adding edit button and listbox

--- ToDo App/functions.py
+++ ToDo App/functions.py
@@ -1,10 +1,12 @@
-def get_todos(filepath="todos.txt"):
+FILEPATH = "todos.txt"
+
+def get_todos(filepath=FILEPATH):
     """ Read the todos text file."""
     with open(filepath, 'r') as file:
             todos = file.readlines()
     return todos
 
-def write_todos(todos, filepath="todos.txt"):
+def write_todos(todos, filepath=FILEPATH):
     """ Write the to-do items list in the text file."""
     with open(filepath, 'w') as file:
             file.writelines(todos)
